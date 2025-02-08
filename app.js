c6752edfbf4fd52b4dfdf4b005705716e38763b9
JaviMartin2002 <fmarmar172@ieszaidinvergeles.org>
2025-02-08 23:52:43 +0100

    finish

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-//
 const express = require("express");
+const app = express();
 const port = 3000;
 const limit = 5000000000;
-const cluster = require("cluster");
-const totalCPUs = require("os").cpus().length;
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
-  // Fork workers.
-  for (let i = 0; i < totalCPUs; i++) {
-  cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-  console.log(`worker ${worker.process.pid} died`);
-  console.log("Let's fork another worker!");
-  cluster.fork();
-  });
-} else {
-  const app = express();
-  console.log(`Worker ${process.pid} started`);
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
-  });
-  app.get("/api/:n", function (req, res) {
+});
+app.get("/api/:n", function (req, res) {
   let n = parseInt(req.params.n);
   let count = 0;
   if (n > limit) n = limit;
@@ -30,8 +13,7 @@ if (cluster.isMaster) {
   count += i;
   }
   res.send(`Final count is ${count}`);
-  });
-  app.listen(port, () => {
+});
+app.listen(port, () => {
   console.log(`App listening on port ${port}`);
-  });
-}
+});
